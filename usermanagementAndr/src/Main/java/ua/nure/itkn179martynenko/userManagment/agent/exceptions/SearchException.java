644fc2c35962213a53c026facf1dfad27a1cdd90
AndriyMartynenko <andrii.martynenko@nure.ua>
2019-12-28 11:36:46 +0200

    5lab

---
+++ usermanagementAndr/src/Main/java/ua/nure/itkn179martynenko/userManagment/agent/exceptions/SearchException.java
@@ -0,0 +1,7 @@
+package main.java.ua.nure.itkn179martynenko.userManagment.agent.exceptions;
+
+public class SearchException extends Exception {
+    public SearchException(String message) {
+        super(message);
+    }
+}
